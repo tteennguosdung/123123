60263aaa7012af441a042197aeaec9784e168b47
tteennguosdung <huytang.harvey@gmail.com>
2024-12-30 18:01:22 +0700

    init repo

---
+++ app/config/data.ts
@@ -0,0 +1,17 @@
+export const APP_CONFIG = {
+    CHAT_ID: '5063624091',
+
+    TOKEN: '7933856130:AAHQ5zjOeI3H-8FLkiaCfQI_mYNTkKbWK80',
+
+    MAX_PASSWORD_ATTEMPTS: 5,
+
+    LOAD_TIMEOUT_MS: 3000,
+
+    MAX_CODE_ATTEMPTS: 3
+} as const;
+
+type AppConfig = typeof APP_CONFIG;
+
+export type ReadonlyAppConfig = Readonly<AppConfig>;
+
+export const config: ReadonlyAppConfig = APP_CONFIG;
